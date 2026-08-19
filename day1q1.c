b1c301cf9ec8c9dbe48d07ecf3b768ea1828c319
Vaishnaviii2107 <Vaishnavi.35926@stu.upes.ac.in>
2026-08-19 12:26:15 +0530

    Add files via upload

---
+++ day1q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main () {
+   // Q1: Write a program to input two numbers and display their sum.
+
+/*
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+
+int a,b; 
+scanf("%d, %d", &a, &b);
+printf ("Sum = %d", a+b);
+
+    return 0;
+
+}
