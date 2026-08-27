f602d0f05cb70025420b191aabf13ca2419a2a87
Vaishnaviii2107 <Vaishnavi.35926@stu.upes.ac.in>
2026-08-27 11:20:37 +0530

    Add files via upload

---
+++ day18q1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{ 
+   // Q35: Write a program to print all factors of a given number.
+
+/*
+Sample Test Cases:
+Input 1:
+6
+Output 1:
+1 2 3 6
+
+Input 2:
+10
+Output 2:
+1 2 5 10
+
+*/
+    int n;
+    printf("Enter n to get all the factors : ");
+    scanf("%d",&n);
+    
+    for(int i=1;i<=n;i++) {
+        if(n%i==0) {
+            printf ("%d",i);
+        }
+    }
+
+    return 0;
+}
